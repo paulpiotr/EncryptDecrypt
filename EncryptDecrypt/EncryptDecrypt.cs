f95ff1ea541c95fd0827cb0b201bf0cbf10c42dd
Piotr Paul <piotr.paul@gmail.com>
2020-12-01 13:20:11 +0100

    Initialize

--- EncryptDecrypt/EncryptDecrypt.cs
+++ EncryptDecrypt/EncryptDecrypt.cs
@@ -30,24 +30,24 @@ namespace EncryptDecrypt
         {
             try
             {
-                MD5CryptoServiceProvider mD5CryptoServiceProvider = new MD5CryptoServiceProvider();
-                byte[] key = Encoding.UTF8.GetBytes(salt);
+                var mD5CryptoServiceProvider = new MD5CryptoServiceProvider();
+                var key = Encoding.UTF8.GetBytes(salt);
                 key = mD5CryptoServiceProvider.ComputeHash(key);
-                using (Aes aes = Aes.Create())
+                using (var aes = Aes.Create())
                 {
                     using (ICryptoTransform iCryptoTransform = aes.CreateEncryptor(key, aes.IV))
                     {
-                        using (MemoryStream memoryStream = new MemoryStream())
+                        using (var memoryStream = new MemoryStream())
                         {
-                            using (CryptoStream cryptoStream = new CryptoStream(memoryStream, iCryptoTransform, CryptoStreamMode.Write))
+                            using (var cryptoStream = new CryptoStream(memoryStream, iCryptoTransform, CryptoStreamMode.Write))
                             {
-                                using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
+                                using (var streamWriter = new StreamWriter(cryptoStream))
                                 {
                                     streamWriter.Write(text);
                                 }
-                                byte[] iv = aes.IV;
-                                byte[] decryptedContent = memoryStream.ToArray();
-                                byte[] result = new byte[iv.Length + decryptedContent.Length];
+                                var iv = aes.IV;
+                                var decryptedContent = memoryStream.ToArray();
+                                var result = new byte[iv.Length + decryptedContent.Length];
                                 Buffer.BlockCopy(iv, 0, result, 0, iv.Length);
                                 Buffer.BlockCopy(decryptedContent, 0, result, iv.Length, decryptedContent.Length);
                                 return Convert.ToBase64String(result);
@@ -72,23 +72,23 @@ namespace EncryptDecrypt
         {
             try
             {
-                byte[] fromBase64String = Convert.FromBase64String(text);
-                byte[] iv = new byte[16];
-                byte[] cipher = new byte[fromBase64String.Length - iv.Length];
+                var fromBase64String = Convert.FromBase64String(text);
+                var iv = new byte[16];
+                var cipher = new byte[fromBase64String.Length - iv.Length];
                 Buffer.BlockCopy(fromBase64String, 0, iv, 0, iv.Length);
                 Buffer.BlockCopy(fromBase64String, iv.Length, cipher, 0, cipher.Length);
-                byte[] key = Encoding.UTF8.GetBytes(salt);
-                MD5CryptoServiceProvider mD5CryptoServiceProvider = new MD5CryptoServiceProvider();
+                var key = Encoding.UTF8.GetBytes(salt);
+                var mD5CryptoServiceProvider = new MD5CryptoServiceProvider();
                 key = mD5CryptoServiceProvider.ComputeHash(key);
-                using (Aes aes = Aes.Create())
+                using (var aes = Aes.Create())
                 {
                     using (ICryptoTransform iCryptoTransform = aes.CreateDecryptor(key, iv))
                     {
-                        using (MemoryStream memoryStream = new MemoryStream(cipher))
+                        using (var memoryStream = new MemoryStream(cipher))
                         {
-                            using (CryptoStream cryptoStream = new CryptoStream(memoryStream, iCryptoTransform, CryptoStreamMode.Read))
+                            using (var cryptoStream = new CryptoStream(memoryStream, iCryptoTransform, CryptoStreamMode.Read))
                             {
-                                using (StreamReader streamReader = new StreamReader(cryptoStream))
+                                using (var streamReader = new StreamReader(cryptoStream))
                                 {
                                     return streamReader.ReadToEnd();
                                 }
@@ -113,7 +113,7 @@ namespace EncryptDecrypt
         {
             try
             {
-                DirectoryInfo directoryInfo = new DirectoryInfo(path ?? AppDomain.CurrentDomain.BaseDirectory);
+                var directoryInfo = new DirectoryInfo(path ?? AppDomain.CurrentDomain.BaseDirectory);
                 while (null != directoryInfo && !directoryInfo.GetFiles((null != file ? file : "public.rsa")).Any())
                 {
                     directoryInfo = directoryInfo.Parent;
